f1d29974d87a5e23d2bdb3b761f5226033c8e0cf
Shukor, Halim <shukor.h@duerr-ndt.com>
2022-08-13 12:59:21 +0800

    refix logic structure

--- 14. Longest Common Prefix/14_Longest_Common_Prefix.py
+++ 14. Longest Common Prefix/14_Longest_Common_Prefix.py
@@ -7,9 +7,10 @@ class Solution:
         for word in words[1:]:
             while word[:len(prefix)] != prefix:
                 prefix = prefix[:-1]
-            if not prefix:
-                return ""
+                if not prefix:
+                    return ""
         return prefix
+
 
 x = Solution()
 print(x.longestCommonPrefix(words=["flower", "flow", "flight"]))
